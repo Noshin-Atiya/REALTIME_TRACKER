a092d1781ebe2aa133caa8053689c7b3ba13b07e
Noshin-Atiya <noshinatiya3@gmail.com>
2025-06-01 20:53:51 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const socket = io();
+
+let userId = prompt("Enter your name or ID"); // Optional, to identify each user
+
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(
+        (position) => {
+            const { latitude, longitude } = position.coords;
+            socket.emit("send-location", { id: userId || socket.id, latitude, longitude });
+        },
+        (error) => {
+            console.error(error);
+        },
+        {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0,
+        }
+    );
+}
+
+const map = L.map("map").setView([0, 0], 16);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "Real-Time Tracker",
+}).addTo(map);
+
+const markers = {};
+
+socket.on("receive-location", (data) => {
+    const { id, latitude, longitude } = data;
+    map.setView([latitude, longitude]);
+
+    if (markers[id]) {
+        markers[id].setLatLng([latitude, longitude]);
+    } else {
+        markers[id] = L.marker([latitude, longitude]).addTo(map).bindPopup(id).openPopup();
+    }
+});
+
+socket.on("user-disconnected", (id) => {
+    if (markers[id]) {
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
+});
